b7001a6e87c5bea411bad481c55ebcbae6dcc9d8
leedongwon6862 <ckqkrhf6862@gmail.com>
2024-04-12 12:31:27 +0900

    start

---
+++ src/main/java/com/example/JumptoSpring/JumptoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.JumptoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumptoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumptoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JumptoSpring/JumptoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JumptoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumptoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
